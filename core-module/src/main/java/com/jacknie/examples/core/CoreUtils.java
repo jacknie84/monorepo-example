7f214d6664e585bce37ded4697893b96a89a622a
jacknie <jacknie8407@gmail.com>
2022-03-26 12:56:06 +0900

    Init project

---
+++ core-module/src/main/java/com/jacknie/examples/core/CoreUtils.java
@@ -0,0 +1,9 @@
+package com.jacknie.examples.core;
+
+public class CoreUtils {
+
+    public static void doSomething() {
+        System.out.println("doSomething");
+    }
+
+}
---
+++ frontend-app/src/main/java/com/jacknie/examples/frontend/FrontendApp.java
@@ -0,0 +1,11 @@
+package com.jacknie.examples.frontend;
+
+import com.jacknie.examples.core.CoreUtils;
+
+public class FrontendApp {
+
+    public static void main(String[] args) {
+        CoreUtils.doSomething();
+    }
+
+}
